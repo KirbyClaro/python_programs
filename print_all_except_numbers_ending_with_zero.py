18c038c105e1be10b5cd74ac8bb5c9d6583daea2
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 00:36:01 +0800

    Creating a program that prints only the number that doesn't end in zero from one to one hundred

---
+++ print_all_except_numbers_ending_with_zero.py
@@ -0,0 +1,4 @@
+#Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+#use a for statement
+#if else statements for picking only the numbers ending in zero
+#print the numbers not ending in zero
