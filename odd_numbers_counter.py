d1f168083f52c640bb59f0ec76420ffdb328e280
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 00:24:53 +0800

    added odd numbers count and used a range with if else statement and then display the result

--- odd_numbers_counter.py
+++ odd_numbers_counter.py
@@ -2,3 +2,12 @@
 #input ten numbers
 #add count to odd_numbers_count if number is odd
 #print odd_numbers_count
+
+odd_numbers_count = 0
+
+for i in range(10):
+    numbers = int(input(f"Enter numbers {i+1}: "))
+    if numbers % 2 != 0:
+        odd_numbers_count += 1
+
+print(f"The number of odd numbers entered is: {odd_numbers_count}")
