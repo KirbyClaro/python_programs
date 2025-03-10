d62fddfb91188e72d5cc1876d0a7a96ebe351b5a
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-10 23:31:37 +0800

    added the formula and prints the result

--- adding_two_numbers.py
+++ adding_two_numbers.py
@@ -6,4 +6,7 @@
 first_num = int(input("Enter the first number: "))
 second_num = int(input("Enter the second number: "))
 
+sum = first_num + second_num
+
+print("The sum of", first_num, "and", second_num, "is:", sum)
 
