c836f505eab3f39ce39a8323a76fe24d97dc73e6
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 00:13:35 +0800

    fixed the indentions and added comments for code readability

--- sum_of_ten_numbers.py
+++ sum_of_ten_numbers.py
@@ -3,11 +3,16 @@
 #add all the numbers
 #print all the numbers
 
+#set the value to 0
 total_of_numbers = 0
+
+#ask the user to input 10 numbers
 print("Enter 10 numbers:")
 
+#loop through the 10 numbers and add them to the total
 for i in range(10):
     numbers = int(input(f"Enter number {i+1}: "))
     total_of_numbers += numbers
 
+#print the sum of all the numbers
 print("Sum of all numbers:", total_of_numbers)
