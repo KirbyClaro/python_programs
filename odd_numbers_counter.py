49ee10a4a4ea19feb8c28bced0e08a292b19ced6
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 00:21:45 +0800

    Creating a program that counts how many are odd numbers from the ten numbers inputted

---
+++ odd_numbers_counter.py
@@ -0,0 +1,4 @@
+#Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+#input ten numbers
+#add count to odd_numbers_count if number is odd
+#print odd_numbers_count
