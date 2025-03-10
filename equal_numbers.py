b258fc7100343f18c16dfacb32cd43173363a0ac
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-10 23:24:00 +0800

    Added comments for better code readability

--- equal_numbers.py
+++ equal_numbers.py
@@ -3,10 +3,13 @@
 #Compare numbers
 #Print output/result
 
+#Input numbers
 first_num = int(input("Input first number: "))
 second_num = int(input("Input second number: "))
 
+#Output numbers
 if first_num == second_num:
     print("Equal")
+#Added elif if the numbers are not equal it still prints
 elif 
     print("Not equal")
