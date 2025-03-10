11ef98b2dd15175d5f7e2639d70cbc90a7e1f699
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 00:31:41 +0800

    added comments for code readability

--- print_even_numbers_zero_to_hundred.py
+++ print_even_numbers_zero_to_hundred.py
@@ -3,7 +3,9 @@
 #if else statements for picking only the even numbers
 #print the even numbers
 
-
+#use range to determine the number of numbers 
 for i in range(101)
+    #check if the number is even by checking if it's divisible by 2 without leaving a remainder
     if i % 2 == 0:
+        #if the number is even, print it
         print(i)
