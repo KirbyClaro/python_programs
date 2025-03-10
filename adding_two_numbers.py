c653e572c2a34a2b2bfc307960a415510a939712
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-10 23:32:56 +0800

    added comments for code readability

--- adding_two_numbers.py
+++ adding_two_numbers.py
@@ -1,12 +1,14 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
-#Input numbers
+#input numbers
 #add two numbers
 #print the sum of the two numbers
 
+#input numbers
 first_num = int(input("Enter the first number: "))
 second_num = int(input("Enter the second number: "))
 
+#add two numbers
 sum = first_num + second_num
 
+#print the sum of the two numbers
 print("The sum of", first_num, "and", second_num, "is:", sum)
-
