12fae024e1d9358f10e2f446768094b8a7b4d653
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-10 23:09:08 +0800

    Added first and second number input

--- bigger_number.py
+++ bigger_number.py
@@ -1,4 +1,7 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 #Input numbers
 #Add numbers
-#print result
+#print 
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
