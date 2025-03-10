9dc8d4181d597d23fb6a12aaeef018eb2b1d4312
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-10 23:49:34 +0800

    added comments for improved code readability

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -3,9 +3,12 @@
 #multiply the two numbers
 #print the product of the two numbers
 
+#input two numbers
 first_number = int(input("Input the first number: "))
 second_number = int(input("Input the second number: "))
 
+#multiply the two numbers
 product = first_number * second_number
 
+#print the product of the two numbers
 print("The product of", first_number, "and", second_number, "is:", product)
