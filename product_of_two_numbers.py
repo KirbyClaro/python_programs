5934739e0c798fd08b7a5888c53e346a18362c59
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-10 23:35:57 +0800

    Creating a program that prints the product of two numbers

---
+++ product_of_two_numbers.py
@@ -0,0 +1,4 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+#input two numbers
+#multiply the two numbers
+#print the product of the two numbers
