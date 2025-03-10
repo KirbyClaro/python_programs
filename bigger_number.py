ea51ae7ab35fcf83f22f54ab36aa5964ac3b31f3
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-10 23:12:59 +0800

    Changed the variable

--- bigger_number.py
+++ bigger_number.py
@@ -1,9 +1,11 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 #Input numbers
 #Add numbers
-#print 
+#Print 
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+#Input numbers
+first_num = int(input("Enter the first number: "))
+second_num = int(input("Enter the second number: "))
 
-print ("The bigger number is", max(num1, num2))
+#Output
+print ("The bigger number is", max(first_num, second_num))
