c481f5b16051e0410d3ac47405e3e5f91c719521
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-10 23:41:11 +0800

    Creating a program that ask the user to input two numbers and displays the result with decimal point

---
+++ qoutient_of_two_numbers_with_decimals.py
@@ -0,0 +1,4 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+#input two numbers with decimal point
+#divide the numbers
+#print result of the program
