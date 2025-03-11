37c12cea8af594891c531c08394fa0528f2e3ae8
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 11:47:33 +0800

    added the formula for remainder and prints the remainder

--- divide_two_number_print_remainder.py
+++ divide_two_number_print_remainder.py
@@ -6,3 +6,7 @@
 
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: ")) 
+
+remainder = first_number % second_number
+
+print("The remainder when", first_number, "is divided by", second_number, "is:", remainder)
