4a0b9c77c01482d68ecb42765073a73f56c0aa5a
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 11:32:21 +0800

    added the formula for substraction and the display output and comments for code readability

--- difference_of_two_numbers.py
+++ difference_of_two_numbers.py
@@ -3,5 +3,12 @@
 #subtract two numbers
 #print the difference of the two numbers
 
+#input two numbers
 first_number = int(input("Enter the First number: "))
 second_number = int(input("Enter the Second number: "))
+
+#subtract two numbers
+difference = first_number - second_number
+
+#output the difference
+print("The difference is:", difference)
