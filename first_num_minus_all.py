03e07c52b70b0af0f822647e796f164426b2747a
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 12:22:59 +0800

    added result and first number inputs

--- first_num_minus_all.py
+++ first_num_minus_all.py
@@ -3,3 +3,7 @@
 #add range for nine remaining numbers and add all of the remaining numbers
 #minus first number to the remaining numbers
 #print the result of the first number minus all of the remaining numbers
+
+first_number = int(input("Enter the first number: "))
+result = first_number 
+
