5f92a7fc08fe644c6ba40cdc71a6e4a1857f355f
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 11:44:06 +0800

    Creating a program that divide two numbers and print the remainder

---
+++ divide_two_number_print_remainder.py
@@ -0,0 +1,5 @@
+#Prog05B2: Create a program that ask user to input 2 numbers. Print the remainder 
+# when the first number is divided by the second number.
+#input two numhbers 
+#divide the numbers and print the remainder 
+#print the remainder
