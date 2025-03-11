62c9887eeeba1afb271fd1a412d6f03dedee0318
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 23:01:50 +0800

    fixed the variables and logic

--- display_all_numbers_and_for_duplicates_only_first_occurance.py
+++ display_all_numbers_and_for_duplicates_only_first_occurance.py
@@ -4,20 +4,21 @@
 #create list for the numbers
 #print all numbers
 #find and display first occurrence of each number
+
 numbers = []
 
 print("Enter 10 numbers:")
 for i in range(10):
-    numbers_ten = int(input(f"Enter each number {i+1}: "))
-    
+    numbers_ten = int(input(f"Enter number {i+1}: "))
+    numbers.append(numbers_ten)
 
-print("all numbers entered:")
+print("\nAll numbers entered:")
 for number in numbers:
     print(number)
 
 print("\nNumbers (showing only first occurrence):")
-unique_numbers = [] 
-for numbers in numbers:
-    if numbers not in unique_numbers:
-        print(numbers_ten)
-        unique_numbers.append(numbers_ten)
+unique_numbers = []
+for number in numbers:
+    if number not in unique_numbers:
+        print(number)
+        unique_numbers.append(number)
