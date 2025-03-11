ccbeea55960d051f72e9d4a7f843ca656c4524e1
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 11:30:04 +0800

    Creating a program that prints the difference between the two numbers

---
+++ difference_of_two_numbers.py
@@ -0,0 +1,4 @@
+#Prog03B2: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+#input two numbers
+#subtract two numbers
+#print the difference of the two numbers
