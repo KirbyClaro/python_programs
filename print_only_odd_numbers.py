bd66650c12a5d8f830a21d75c87bda5989479489
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 12:41:37 +0800

    Creating a program that prints all odd numbers starting from zero to one hundred using while loop

---
+++ print_only_odd_numbers.py
@@ -0,0 +1,4 @@
+#Prog08B2: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+#use while loop with range to print all the numbers
+#use if statement to check if numbers are odd
+#print only the odd numbers starting from 0 to 100
