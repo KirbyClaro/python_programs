5c032bd0029f3389ddf63ffeaac8ee49b36a81b0
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 12:21:58 +0800

    Creating a program that ask a user to input 10 numbers and minus a first number to all the remaining number then prints the result

---
+++ first_num_minus_all.py
@@ -0,0 +1,5 @@
+#Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+#input first number
+#add range for nine remaining numbers and add all of the remaining numbers
+#minus first number to the remaining numbers
+#print the result of the first number minus all of the remaining numbers
