9af7d4cce5dc7f6324e145dd2bfb86135235cfdb
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 13:00:41 +0800

    added comments to improve code readability

--- print_all_numbers_between_first_second_number.py
+++ print_all_numbers_between_first_second_number.py
@@ -3,9 +3,12 @@
 #use range to determine the numbers in between 
 #print the numbers between two numbers
 
+#input two numbers
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
 
+#use range to determine the numbers in between and prints the numbers between two numbers
 print(f"The numbers between {first_number} and {second_number} are:")
+
 for i in range(min(first_number, second_number), max(first_number, second_number)) :
     print(i)
