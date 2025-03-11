6bbf8299e3d7b5af4f5db40e640677b11f6aa9b6
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 11:21:34 +0800

    Creating a program that ask for two numbers and determine if they are not equal

---
+++ not_equal_numbers.py
@@ -0,0 +1,4 @@
+#Prog02B2: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+#input two numbers
+#determine if two numbers are not equal
+#print "Not Equal" if not equal
