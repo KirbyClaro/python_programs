084caea071a09f440657fb5ed38e4b5988e04a37
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 12:37:27 +0800

    fixed syntax errors and initialized  the even number count and also added a value to add even numbers then displayed the result

--- print_only_even_numbers.py
+++ print_only_even_numbers.py
@@ -3,6 +3,11 @@
 #add the even numbers to the count
 #print all the even numbers counted
 
+even_numbers_count = 0
+
 for i in range(10):
-    numbers = int(input|(f"Enter the number{i+1}: "))
+    numbers = int(input(f"Enter the number {i+1}: "))
     if numbers % 2 == 0:
+        even_numbers_count += 1
+
+print(f"The count of even numbers is: {even_numbers_count}")
