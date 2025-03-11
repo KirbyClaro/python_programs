f8db845206e5fcf537f38fe56e0cfb3e19578908
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 11:23:08 +0800

    added inputs

--- not_equal_numbers.py
+++ not_equal_numbers.py
@@ -2,3 +2,7 @@
 #input two numbers
 #determine if two numbers are not equal
 #print "Not Equal" if not equal
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
