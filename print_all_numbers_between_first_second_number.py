a24eee1bb6a0d24c750f90496cb5322584e361ab
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 12:56:23 +0800

    Creating a program that ask user to input two numbers and prints all the number in between the two numbers inputted

---
+++ print_all_numbers_between_first_second_number.py
@@ -0,0 +1,4 @@
+#Prog10B2: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+#input two numbers
+#use range to determine the numbers in between 
+#print the numbers between two numbers
