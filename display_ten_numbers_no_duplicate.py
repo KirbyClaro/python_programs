5cffe3252e9a73cc5647d958db6ea61f88c72977
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 21:00:19 +0800

    added range and conditions --- code not complete yet

--- display_ten_numbers_no_duplicate.py
+++ display_ten_numbers_no_duplicate.py
@@ -2,3 +2,10 @@
 #input ten numbers 
 #create if statement to only display numbers without duplicates
 #display all numbers that don't have duplicate
+
+numbers = 0
+
+for i in range(10):
+    numbers = int(input(f"Enter the number {i+1}:"))
+    if numbers not in (numbers_list + [None]):
+        print(numbers)
