bb92d8f61311474cf6218a7e813a19848460fc6b
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 22:20:03 +0800

    Creating a program that ask user to input 10 numbers. that display all numbers. For numbers with duplicate, display only the first entry.

---
+++ display_all_numbers_and_for_duplicates_only_first_occurance.py
@@ -0,0 +1,7 @@
+#Prog02B3: Create a program that ask user to input 10 numbers. 
+# Display all numbers. For numbers with duplicate, display only the first entry.
+#input ten numbers
+#create list for the numbers
+#print all numbers
+#find and display first occurrence of each number
+
