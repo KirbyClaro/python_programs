7a6569f1b78ac8f46c7efe37f3fbab9af7fb0680
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 21:18:47 +0800

    added comments to improve code readability

--- display_ten_numbers_no_duplicate.py
+++ display_ten_numbers_no_duplicate.py
@@ -3,12 +3,15 @@
 #create if statement to only display numbers without duplicates
 #display all numbers that don't have duplicate
 
+#initialize numbers
 numbers = []
 
+#get user input for 10 numbers and store them in the list 'numbers'
 print("Enter 10 Numbers")
 for i in range(10):
     numbers_input = int(input(f"Enter the number {i+1}:"))
     numbers.append(numbers_input)
 
+#find and display unique numbers
 unique_numbers = [numbers_input for numbers_input in numbers if numbers.count(numbers_input) == 1]
 print("Unique numbers:", unique_numbers)
