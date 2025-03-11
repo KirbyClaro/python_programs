9059d4f28e38a4f23b4424ec0521b0f5da87b4a0
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 12:43:15 +0800

    added i as range and used while statement to stop at 100 and used if statement to check if number is odd then prints it

--- print_only_odd_numbers.py
+++ print_only_odd_numbers.py
@@ -2,3 +2,10 @@
 #use while loop with range to print all the numbers
 #use if statement to check if numbers are odd
 #print only the odd numbers starting from 0 to 100
+
+i = 0
+
+while i <= 100:
+    if i % 2 != 0:
+        print(i)
+    i += 1
