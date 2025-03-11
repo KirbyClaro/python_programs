546f12af17ab2c1d4e6018768cc163d6114b1643
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 12:38:23 +0800

    added comments for code readability and fixed syntax errors with pseudocode

--- print_only_even_numbers.py
+++ print_only_even_numbers.py
@@ -1,13 +1,17 @@
-#Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+#Prog07B2: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 #input ten numbers using range 
 #add the even numbers to the count
 #print all the even numbers counted
 
+#initialize count of even numbers
 even_numbers_count = 0
 
+#prompt user to enter 10 numbers and count even numbers
 for i in range(10):
     numbers = int(input(f"Enter the number {i+1}: "))
+    #check if number is even and increment count if it is
     if numbers % 2 == 0:
         even_numbers_count += 1
 
+#print the count of even numbers entered
 print(f"The count of even numbers is: {even_numbers_count}")
