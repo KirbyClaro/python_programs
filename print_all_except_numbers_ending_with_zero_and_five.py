a9133b025afb875330de991de466ae98e4c53716
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 12:53:04 +0800

    added initialization for i and created the if statement so that numbers ending in zero and 5 are not included

--- print_all_except_numbers_ending_with_zero_and_five.py
+++ print_all_except_numbers_ending_with_zero_and_five.py
@@ -2,3 +2,10 @@
 #use while statement to print only the numbers starting from 0 to 100
 #create if statement to not print the numbers ending in zero or ending five
 #print the numbers
+
+i = 0
+
+while i <= 100:
+    if i % 10 != 0 and i % 10 != 5:
+        print(i)
+    i += 1
