19e717c97c163873f959f3f6564aadc02278297e
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 20:56:11 +0800

    Creating a program that ask user for ten numbers and only displays numbers that don't have duplicates

---
+++ display_ten_numbers_no_duplicate.py
@@ -0,0 +1,4 @@
+#Prog01B3: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+#input ten numbers 
+#create if statement to only display numbers without duplicates
+#display all numbers that don't have duplicate
