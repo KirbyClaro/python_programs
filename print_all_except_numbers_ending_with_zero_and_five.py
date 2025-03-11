2268a29e65cceb3cf752716739b203f02a69bcab
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 12:54:02 +0800

    added comment for code readability

--- print_all_except_numbers_ending_with_zero_and_five.py
+++ print_all_except_numbers_ending_with_zero_and_five.py
@@ -3,9 +3,13 @@
 #create if statement to not print the numbers ending in zero or ending five
 #print the numbers
 
+#initialize 
 i = 0
 
+#use while loop to print numbers from 0 to 100
 while i <= 100:
+    #check if the number is not ending in zero or five and print it 
     if i % 10 != 0 and i % 10 != 5:
         print(i)
+    #increment the counter
     i += 1
