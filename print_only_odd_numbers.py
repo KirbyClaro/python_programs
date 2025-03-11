b1454b8d6480fbb8a9d3136d5f180ae163f6a170
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 12:44:11 +0800

    added comments for code readability

--- print_only_odd_numbers.py
+++ print_only_odd_numbers.py
@@ -3,9 +3,13 @@
 #use if statement to check if numbers are odd
 #print only the odd numbers starting from 0 to 100
 
+#initialize counter variable i to 0
 i = 0
 
+#use while loop to iterate until i is less than or equal to 100
 while i <= 100:
+    #check if the number is odd by checking if it's divisible by 2 without leaving a remainder
     if i % 2 != 0:
         print(i)
+    #increment i by 1 to move to the next number in the loop
     i += 1
