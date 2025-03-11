6965812f0941d976c24a90041a5093cecd505769
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 12:27:12 +0800

    added comments and syntax for better code readability

--- first_num_minus_all.py
+++ first_num_minus_all.py
@@ -4,11 +4,16 @@
 #minus first number to the remaining numbers
 #print the result of the first number minus all of the remaining numbers
 
+#input first number
 first_number = int(input("Enter the first number: "))
+
 result = first_number 
 
+#add range for nine remaining numbers and add all of the remaining numbers
 for i in range(9):
     numbers = int(input(f"Enter the number {i+1}:"))
+    #formula for the first number minus all of the remaining numbers
     result -= numbers
 
+#print the result of the first number minus all of the remaining numbers
 print("The result of", first_number, "minus all of the remaining numbers is:", result)
