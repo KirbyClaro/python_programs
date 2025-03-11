249f7816e18a4ecc69bc715786486968a8a01cab
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-11 11:34:49 +0800

    Creating a program that finds the qoutient of two numbers without decimal point

---
+++ qoutient_of_two_numbers_without_decimals.py
@@ -0,0 +1,4 @@
+#Prog04B2: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+#input the two numbers
+#divide the numbers 
+#print the quotient of the two numbers
