39f14662e977809327a6b453ce34ade1428c67e9
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-12 22:36:07 +0800

    Creating a program that ask user to input a number continue till user input is invalid display numbers from lowest to highest

---
+++ display_number_lowest_highest.py
@@ -0,0 +1,5 @@
+#Prog05B3: Create a program that ask user to input a number, continue asking until the user input is invalid. 
+# Display the number from lowest to highest. Clue: sort() function
+#ask user to input till user input is invalid 
+#sort values inputed by user input from lowest to highest
+#display the number from lowest to highest
