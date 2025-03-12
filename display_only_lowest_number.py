95e6feb213bb0da8ee0ac6957750483b5e328c2f
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-12 22:28:06 +0800

    progress still working oh it added if statement with number = float to hold the input and lowest number value

--- display_only_lowest_number.py
+++ display_only_lowest_number.py
@@ -1,15 +1,24 @@
 #Prog04B3: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 #ask user to input till invalid
 #only display the lowest number
-number = []
+
+lowest_num = float('inf')
+
 while True:
     try:
         # Input a number
-        number = int(input("Enter a number (Enter an invalid input to stop): "))
-        number.append(number)
+        number_input = input("Enter a number (Enter an invalid input to stop): ")
+        
+        if number_input.strip() == "":
+            break
+
+        number = float(number_input)
+
+        lowest_number = min(lowest_number, number)
+
     except ValueError:
         print("Invalid input. Please enter a valid number.")
         continue
-    break
+
 
 lowest_number = min(number)
