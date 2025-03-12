55e2db87d0648c70aa0307295637dd4918e46425
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-12 22:23:31 +0800

    added number list and append to add numbers

--- display_only_lowest_number.py
+++ display_only_lowest_number.py
@@ -1,11 +1,12 @@
 #Prog04B3: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 #ask user to input till invalid
 #only display the lowest number
-
+number = []
 while True:
     try:
         # Input a number
         number = int(input("Enter a number (Enter an invalid input to stop): "))
+        number.append(number)
     except ValueError:
         print("Invalid input. Please enter a valid number.")
         continue
