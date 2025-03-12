d1948ddb6d45337ddf1abb076cc61b6d938a1688
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-12 22:22:50 +0800

    added while statement from previous code and added lowest number with min statement so it shows the lowest number

--- display_only_lowest_number.py
+++ display_only_lowest_number.py
@@ -1,3 +1,14 @@
 #Prog04B3: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 #ask user to input till invalid
 #only display the lowest number
+
+while True:
+    try:
+        # Input a number
+        number = int(input("Enter a number (Enter an invalid input to stop): "))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        continue
+    break
+
+lowest_number = min(number)
