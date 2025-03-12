8d8c42ccac69fff4e81635b246ff66a04cebfebb
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-12 22:19:55 +0800

    Creating a program that ask user to input till user input is invalid. display only the lowest number

---
+++ display_only_lowest_number.py
@@ -0,0 +1,3 @@
+#Prog04B3: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+#ask user to input till invalid
+#only display the lowest number
