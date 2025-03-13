098ce8ae9412064b83cf7305081fba7b4738bb9f
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-13 21:35:57 +0800

    Creating a program that ask user to input a number till user input is invalid and display the average

---
+++ display_average_all_numbers.py
@@ -0,0 +1,6 @@
+#Prog05: Create a program that ask user to input a number, 
+# continue asking until the user input is invalid. 
+# Display the average.
+#ask user to input a number until user input is invalid
+#compute for the average
+#display the average
