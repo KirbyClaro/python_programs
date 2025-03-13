60390d5199c114df752269daf93631a3ec461311
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-13 20:52:27 +0800

    added comments for code readability

--- display_duplicate_numbers.py
+++ display_duplicate_numbers.py
@@ -3,12 +3,15 @@
 #ask user to input 10 numbers
 #seperate all numbers with duplicates
 
+#initialize numbers list 
 numbers = []
 
+#ask user to input 10 numbers and store them in the list 'numbers'
 for i in range(10):
     numbers_input = int(input(f"Enter number {i+1}: "))
     numbers.append(numbers_input)
 
+#find all numbers that have duplicate
 duplicates = {num for num in numbers if numbers.count(num) > 1}
 
 print("Duplicate numbers:", duplicates)
