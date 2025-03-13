52aa783dcd5b933d445f6cbf4a53d011bbb16349
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-13 21:15:52 +0800

    Creating a program that ask user to input a number till user input is invalid and display the highest number

---
+++ display_highest_number.py
@@ -0,0 +1,6 @@
+#Prog03B4: Create a program that ask user to input a number, 
+# continue asking until the user input is invalid. 
+# Display the highest number
+#input number till user input is invalid
+#find the highest number
+#display the highest number
