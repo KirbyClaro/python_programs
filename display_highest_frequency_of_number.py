27c0058a0b4c57f10cc86c0cdfc2d13a7b9d988c
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-13 21:12:46 +0800

    added comments for code readability and fix print value for error

--- display_highest_frequency_of_number.py
+++ display_highest_frequency_of_number.py
@@ -5,8 +5,10 @@
 #count occurance of numbers
 #display number with the most number of duplicate
 
+# Initialize an empty list to store the numbers.
 numbers = []
 
+# Ask user to input numbers until user input is invalid.
 while true:
     try:
         number_input = int(input("Enter a number (press Enter to stop): "))
@@ -14,11 +16,15 @@ while true:
     except ValueError:
         break
 
+# If there are any numbers in the list, calculate the most frequent number.
 if numbers:
-    count_dictionary = {number_input: numbers.count(number_input) for number_input in set(numbers)}
+    count_dictionary = {number_input: numbers.count(number_input) 
+                        for number_input in set(numbers)}
 
     most_frequent = max(count_dictionary, key=count_dictionary.get)
 
+    # Display the number with the most number of duplicate.
     print(f"The number with the most number of duplicate is: {most_frequent}")
 else:
-    prindt("No numbers entered.")
+    # Display a message if no numbers were entered.
+    print("No numbers entered.")
