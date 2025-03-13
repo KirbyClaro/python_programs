387b88e4179d9aac4402f7fd782c020558758e44
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-13 20:58:18 +0800

    Creating a program that ask user top input a number till user input is invalid and display the most number of duplicate

---
+++ display_highest_frequency_of_number.py
@@ -0,0 +1,6 @@
+#Prog02B4: Create a program that ask user to input a number, 
+# continue asking until the user input is invalid. 
+# Display the number with the most number of duplicate.
+#ask user to input numbers until user input is invalid
+#count occurance of numbers
+#display number with the most number of duplicate
