854cfefc7cd0de680c29e90e0f3124ec7e0a123b
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-13 21:19:07 +0800

    reused code from the previous code

--- display_highest_number.py
+++ display_highest_number.py
@@ -4,3 +4,14 @@
 #input number till user input is invalid
 #find the highest number
 #display the highest number
+
+# Initialize an empty list to store the numbers.
+numbers = []
+
+# Ask user to input numbers until user input is invalid.
+while True:
+    try:
+        number_input = int(input("Enter a number (press Enter to stop): "))
+        numbers.append(number_input)
+    except ValueError:
+        break
