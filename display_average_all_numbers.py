a374c09c78feb60470b141ba151ce4da03f22f96
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-13 21:37:10 +0800

    reused the code for the user input from the previous program

--- display_average_all_numbers.py
+++ display_average_all_numbers.py
@@ -3,4 +3,15 @@
 # Display the average.
 #ask user to input a number until user input is invalid
 #compute for the average
-#display the average
+#display the 
+
+# Initialize an empty list to store the numbers.
+numbers = []
+
+# Ask user to input numbers until user input is invalid.
+while True:
+    try:
+        number_input = int(input("Enter a number (press Enter to stop): "))
+        numbers.append(number_input)
+    except ValueError:
+        break
