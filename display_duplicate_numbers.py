56334eff0f402908cb3d4ca7a2e38be388935f4f
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-13 20:26:59 +0800

    Creating a program that ask user to input ten numbers and only display numbers that have duplicates

---
+++ display_duplicate_numbers.py
@@ -0,0 +1,4 @@
+#Prog01B4: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+#ask user to input 10 numbers
+#seperate all numbers with duplicates
+#print duplicate numbers
