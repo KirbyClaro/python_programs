2760a3b15a52086d50cc9f7190bf9edd5649ed94
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-20 00:21:52 +0800

    added comments for code readability

--- input_without_dead_space.py
+++ input_without_dead_space.py
@@ -2,8 +2,11 @@
 #fullname with several space characters at the beginning. 
 #Print the input without the spaces in the beginning.
 
+#input the user's name with spaces in the beginning
 user_input_name = input("Enter your name: ")
 
+#remove the spaces from the beginning of the input name
 removed_space = user_input_name.lstrip()
 
+#print the input name without the spaces in the beginning
 print("Output:", removed_space)
