a463b3c0ab1208499c1d80e2153fb86d84af4990
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-20 00:38:57 +0800

    added number input and zfill for adding zero and display

--- input_number_six_digit_format.py
+++ input_number_six_digit_format.py
@@ -1,3 +1,8 @@
-#Prog02: Create a program that ask the user to input a number (0-1000). 
+#Prog02B5: Create a program that ask the user to input a number (0-1000). 
 # Print the number in 6 digit format. Add zeros at the 
 # beginning to complete the 6 digit.
+
+    number_input = int(input("Enter a number (0-1000): "))
+    formatted_number = str(number_input).zfill(6)
+
+    print("The number in 6 digit format is:", formatted_number)
