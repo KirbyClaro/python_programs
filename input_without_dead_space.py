755705eaee84854182b33fd23fd5b8bc2950278d
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-20 00:19:42 +0800

    added input for user name

--- input_without_dead_space.py
+++ input_without_dead_space.py
@@ -1,3 +1,5 @@
 #Prog01B5: Create a program that ask the user to input their 
 #fullname with several space characters at the beginning. 
 #Print the input without the spaces in the beginning.
+
+user_input_name = input("Enter your name: ")
