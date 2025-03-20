1901ced1625de057eb00d8c8c64235eb95b9a49e
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-20 19:15:48 +0800

    Reused the previous code and change the function from .upper to .lower

--- input_name_display_lower_case.py
+++ input_name_display_lower_case.py
@@ -1,2 +1,8 @@
 #Prog04B5: Create a program that ask the user to 
 # input their fullname. Print the input in all lower case.
+
+#input the user's name
+fullname = input("Please enter your fullname: ")
+
+#print the input name in all capital letters
+print("Your name is ", fullname.lower())
