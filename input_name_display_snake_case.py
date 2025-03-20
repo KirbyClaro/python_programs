353eec52a9919943cc5ab1644027f72e2dd4a6ec
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-20 19:54:32 +0800

    reused input and split function from the previous program

--- input_name_display_snake_case.py
+++ input_name_display_snake_case.py
@@ -1,3 +1,9 @@
 #Prog10: Create a program that ask the user 
 # to input their fullname in incorrect casing. 
 # Print the input in snake case.
+
+#input the user's name in incorrect casing
+fullname = input("Please enter your fullname: ")
+
+#convert the input name to pascal case
+split_fullname = fullname.split()
