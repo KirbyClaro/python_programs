ffa1f0910d3b0e1e5cfba4fb1bb80145d391e320
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-20 19:43:29 +0800

    added comment for code readability

--- input_name_display_number_of_characters.py
+++ input_name_display_number_of_characters.py
@@ -2,6 +2,8 @@
 # to input their fullname. 
 # Print the number of characters in the input.
 
+# Ask the user to input their fullname.
 fullname = input("Enter your fullname: ")
 
+# Print the number of characters in the input fullname.
 print("Number of characters:", len(fullname))
