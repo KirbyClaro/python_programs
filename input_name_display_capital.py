a20985666b16775a0137632475a2984e6b94211d
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-20 19:11:28 +0800

    added fullname variable with .upper function. with comments for code readability

--- input_name_display_capital.py
+++ input_name_display_capital.py
@@ -1,6 +1,8 @@
 #Prog03B5: Create a program that ask the user to 
 # input their fullname. Print the input in all capital letter.
 
-input("Please enter your fullname: ")
+#input the user's name
+fullname = input("Please enter your fullname: ")
 
-print(input())
+#print the input name in all capital letters
+print("Your name is ", fullname.upper())
