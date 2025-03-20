ea58b6d9c4c36f79851a325ad36f01be0636ae73
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-20 19:40:36 +0800

    added len to count how many words are there and display and comments for code readability

--- input_sentence_display_number.py
+++ input_sentence_display_number.py
@@ -1,6 +1,14 @@
 #Prog07B5: Create a program that ask the user to input a 
 #complete statement. Print the number of words in the input.
 
+# Input a complete statement.
 sentence = input("Enter a complete statement: ")
 
+# Split the sentence into a list of words.
 split_words = sentence.split()
+
+# Calculate the number of words in the input statement.
+word_count = len(split_words)
+
+# Print the number of words in the input statement.
+print(f"The number of words in the statement is {word_count}")
