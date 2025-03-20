64c83e1b6417a5f1e46bfa306e778993051c55f5
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-20 19:22:40 +0800

    reused the code for input and output change from .lower to .title to display the first letter as capital and the next lowercase for each word. with comments for code readability

--- input_name_display_proper_casing.py
+++ input_name_display_proper_casing.py
@@ -1,3 +1,9 @@
 #Prog05B5: Create a program that ask the user to input 
 # their fullname in incorrect casing. 
 # Print the input in proper casing.
+
+#input the user's name
+fullname = input("Please enter your fullname: ")
+
+#print the input name in correct casing
+print("Your name is ", fullname.title())
