23e83c0b80c16ee4d62dc144a54c5bb0b12e9ce8
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-20 19:48:02 +0800

    added input and then split the input

--- input_name_display_pascal_casing.py
+++ input_name_display_pascal_casing.py
@@ -1,3 +1,7 @@
 #Prog09B5: Create a program that ask the user 
 #to input their fullname in incorrect casing. 
 # Print the input in pascal case.
+
+fullname = input("Please enter your fullname: ")
+
+split_fullname = fullname.split()
