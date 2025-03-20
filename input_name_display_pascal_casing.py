320df2f6248c962846a7cb6d70b57a1d0ced7908
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-20 19:51:26 +0800

    added comment to make it more readable

--- input_name_display_pascal_casing.py
+++ input_name_display_pascal_casing.py
@@ -2,13 +2,17 @@
 #to input their fullname in incorrect casing. 
 # Print the input in pascal case.
 
+#input the user's name in incorrect casing
 fullname = input("Please enter your fullname: ")
 
+#convert the input name to pascal case
 split_fullname = fullname.split()
 
+#concatenate the words in pascal case, capitalizing the first letter of each word
 pascal_case = ""
 for word in split_fullname:
     pascal_case += word[0].upper()
     pascal_case += word[1:].lower()
 
+#print the input in pascal case
 print("Pascal Case:", pascal_case)
