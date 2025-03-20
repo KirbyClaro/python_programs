409077fdd7d0fc27a2cc1a09bc4dbf7561d42c7e
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-20 19:39:39 +0800

    added input and .split function with another variable

--- input_sentence_display_number.py
+++ input_sentence_display_number.py
@@ -1,2 +1,6 @@
 #Prog07B5: Create a program that ask the user to input a 
 #complete statement. Print the number of words in the input.
+
+sentence = input("Enter a complete statement: ")
+
+split_words = sentence.split()
